5eb639711e082111b05ef14b89565615a4ee7a92
anastasiabon <avbondarkova@mail.ru>
2021-07-01 20:38:55 +0300

    Task#2

--- src/index.js
+++ src/index.js
@@ -17,38 +17,41 @@ let walkPic = 0;
 let bottomPressed = false;
 let direction = '';
 
+const canvasSize = 600;
+const picSize = 48;
+
 const img = document.createElement('img');
 img.src = characterWalk;
 
 function keyDownHandler(e) {
   if (e.key === 'Down' || e.key === 'ArrowDown') {
-      bottomPressed = true;
-      direction = 'down';
+    bottomPressed = true;
+    direction = 'down';
   } else if (e.key === 'Up' || e.key === 'ArrowUp') {
-      bottomPressed = true;
-      direction = 'up';
+    bottomPressed = true;
+    direction = 'up';
   } else if (e.key === 'Right' || e.key === 'ArrowRight') {
-      bottomPressed = true;
-      direction = 'right';
+    bottomPressed = true;
+    direction = 'right';
   } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
-      bottomPressed = true;
-      direction = 'left';
+    bottomPressed = true;
+    direction = 'left';
   }
 }
 
 function keyUpHandler(e) {
   if (e.key === 'Down' || e.key === 'ArrowDown') {
-      bottomPressed = false;
-      direction = '';
+    bottomPressed = false;
+    direction = '';
   } else if (e.key === 'Up' || e.key === 'ArrowUp') {
-      bottomPressed = false;
-      direction = '';
+    bottomPressed = false;
+    direction = '';
   } else if (e.key === 'Right' || e.key === 'ArrowRight') {
-      bottomPressed = false;
-      direction = '';
+    bottomPressed = false;
+    direction = '';
   } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
-      bottomPressed = false;
-      direction = '';
+    bottomPressed = false;
+    direction = '';
   }
 }
 
@@ -57,29 +60,47 @@ document.addEventListener('keyup', keyUpHandler);
 
 const getDirection = () => {
   if (!bottomPressed) {
-      return { pX, pY };
+    return { pX, pY };
   }
 
+  const moveRange = 10;
+
   switch (direction) {
-      case 'down':
-          pY += 10;
-          walkPic = 0;
-          break;
-      case 'up':
-          pY -= 10;
-          walkPic = 144;
-          break;
-      case 'right':
-          pX += 10;
-          walkPic = 96;
-          break;
-      case 'left':
-          pX -= 10;
-          walkPic = 48;
-          break;
-      default:
-          pY = 0;
-          pX = 0;
+    case 'down':
+      if (pY >= canvasSize - (picSize + moveRange)) {
+        break;
+      }
+
+      pY += moveRange;
+      walkPic = 0;
+      break;
+    case 'up':
+      if (pY <= 0) {
+        break;
+      }
+
+      pY -= moveRange;
+      walkPic = 144;
+      break;
+    case 'right':
+      if (pX >= canvasSize - (picSize)) {
+        break;
+      }
+
+      pX += moveRange;
+      walkPic = 96;
+      break;
+    case 'left':
+      if (pX <= 0) {
+        break;
+      }
+
+      pX -= moveRange;
+      walkPic = 48;
+      break;
+    default:
+      pY = 0;
+      pX = 0;
   }
 
   cycle = (cycle + 1) % shots;
@@ -94,10 +115,8 @@ const drawImage = (xPos = 0, yPos = 0, walkPos = 0) => {
 
 img.addEventListener('load', () => {
   setInterval(() => {
-      const { pX: xPos, pY: yPos, walkPic: walkPos } = getDirection();
+    const { pX: xPos, pY: yPos, walkPic: walkPos } = getDirection();
 
-      drawImage(xPos, yPos, walkPos);
+    drawImage(xPos, yPos, walkPos);
   }, 120);
 });
-
-
