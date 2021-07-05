b76e1f0d16445edf7cf3ef7be775888974ce0969
anastasiabon <avbondarkova@mail.ru>
2021-07-05 21:55:48 +0300

    Refactoring after review

--- src/index.js
+++ src/index.js
@@ -15,13 +15,11 @@ const spriteH = 48;
 const shots = 3;
 let cycle = 0;
 
-let bottomPressed = false;
-let direction = '';
+let buttonPressed = null;
 
-const canvasSize = 600;
 const picSize = 48;
 
-const centralPos = canvasSize / 2 - picSize / 2;
+const centralPos = canvas.width / 2 - picSize / 2;
 
 let pX = centralPos;
 let pY = centralPos;
@@ -31,34 +29,18 @@ const img = document.createElement('img');
 img.src = characterWalk;
 
 function keyDownHandler(e) {
-  if (e.key === 'Down' || e.key === 'ArrowDown') {
-    bottomPressed = true;
-    direction = 'down';
-  } else if (e.key === 'Up' || e.key === 'ArrowUp') {
-    bottomPressed = true;
-    direction = 'up';
-  } else if (e.key === 'Right' || e.key === 'ArrowRight') {
-    bottomPressed = true;
-    direction = 'right';
-  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
-    bottomPressed = true;
-    direction = 'left';
+  if (
+    e.key === 'Down' || 'ArrowDown' || 'Up' || 'ArrowUp' || 'Right' || 'ArrowRight' || 'Left' || 'ArrowLeft'
+  ) {
+    buttonPressed = e.key;
   }
 }
 
 function keyUpHandler(e) {
-  if (e.key === 'Down' || e.key === 'ArrowDown') {
-    bottomPressed = false;
-    direction = '';
-  } else if (e.key === 'Up' || e.key === 'ArrowUp') {
-    bottomPressed = false;
-    direction = '';
-  } else if (e.key === 'Right' || e.key === 'ArrowRight') {
-    bottomPressed = false;
-    direction = '';
-  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
-    bottomPressed = false;
-    direction = '';
+  if (
+    e.key === 'Down' || 'ArrowDown' || 'Up' || 'ArrowUp' || 'Right' || 'ArrowRight' || 'Left' || 'ArrowLeft'
+  ) {
+    buttonPressed = null;
   }
 }
 
@@ -66,44 +48,44 @@ document.addEventListener('keydown', keyDownHandler);
 document.addEventListener('keyup', keyUpHandler);
 
 const getDirection = () => {
-  if (!bottomPressed) {
+  if (!buttonPressed) {
     return { pX, pY };
   }
 
   const moveRange = 10;
 
-  switch (direction) {
-    case 'down':
-      if (pY >= canvasSize - (picSize + moveRange)) {
+  switch (buttonPressed) {
+    case 'ArrowDown':
+      if (pY >= canvas.height - (picSize + moveRange)) {
         break;
       }
 
       pY += moveRange;
       walkPic = 0;
       break;
-    case 'up':
+    case 'ArrowUp':
       if (pY <= 0) {
         break;
       }
 
       pY -= moveRange;
-      walkPic = 144;
+      walkPic = picSize * 3;
       break;
-    case 'right':
-      if (pX >= canvasSize - picSize) {
+    case 'ArrowRight':
+      if (pX >= canvas.width - picSize) {
         break;
       }
 
       pX += moveRange;
-      walkPic = 96;
+      walkPic = picSize * 2;
       break;
-    case 'left':
+    case 'ArrowLeft':
       if (pX <= 0) {
         break;
       }
 
       pX -= moveRange;
-      walkPic = 48;
+      walkPic = picSize;
       break;
     default:
       return { pX, pY, walkPic };
@@ -115,8 +97,8 @@ const getDirection = () => {
 };
 
 const drawImage = (xPos, yPos, walkPos = 0) => {
-  ctx.clearRect(0, 0, 600, 600);
-  ctx.drawImage(img, cycle * spriteW, walkPos, spriteW, spriteH, xPos, yPos, 48, 48);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(img, cycle * spriteW, walkPos, spriteW, spriteH, xPos, yPos, picSize, picSize);
 };
 
 img.addEventListener('load', () => {
